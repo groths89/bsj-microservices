340b6b18692755ff71d8a7c98cd38d11264a031b
BuildTools <unconfigured@null.spigotmc.org>
2016-04-17 20:44:16 -0400

    Added all directories

---
+++ includes/meta-data.php
@@ -0,0 +1,17 @@
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Baseball Stadium Journey</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="css/vendor/bootstrap.min.css">
+        <style>
+            body {padding-top: 0; padding-bottom: 0;}
+        </style>
+        <link rel="stylesheet" type="text/css" href="css/vendor/bootstrap-theme.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" media="screen,projection" href="cssmap-usa/cssmap-usa.css">
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+        <script type="text/javascript" src="http://cssmapsplugin.com/4/jquery.cssmap.js"></script>
+    </head>
---
+++ includes/navigation.php
@@ -0,0 +1,61 @@
+    <div class="navbar navbar-default header" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand margin" href="#">
+            <img alt="Brand" src="...">
+          </a>
+
+        </div>
+
+      </div>
+      
+    </div>
+    <div class="navbar navbar-default" role="navigation">
+      <div class="container nav-contain">
+        <div class="navbar-header navigation">
+        <nav>
+          <ul class="nav nav-tabs top">
+            <li><a href="#">News</a></li>
+            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Current Season</a>
+
+                <ul class="dropdown-menu sub">
+                  <li><a href="#">Schedule</a></li>
+                  <li><a href="#">Standings</a></li>
+                </ul>
+
+            </li>
+            <li role="presentation" class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Journals</a>
+
+                <ul class="dropdown-menu sub">
+                  <li><a href="#">Citi Field</a></li>
+                  <li><a href="#">Citizens Bank Park</a></li>
+                  <li><a href="#">Fenway Park</a></li>
+                  <li><a href="#">Marlins Park</a></li>
+                  <li><a href="#">Nationals Park</a></li>
+                  <li><a href="#">Oriole Park at Camden Yards</a></li>
+                  <li><a href="#">PNC Park</a></li>
+                  <li><a href="#">Rogers Centre</a></li>
+                  <li><a href="#">Tropicana Field</a></li>
+                  <li><a href="#">Yankee Stadium</a></li>
+                </ul>
+
+            </li>
+          </ul>
+          <ul class="top-right">
+            <li><a data-toggle="modal" href="#LogIn">Login</a></li>
+            <li><a href="#">Register</a></li>
+          </ul>
+        </nav>
+
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+    </div>
